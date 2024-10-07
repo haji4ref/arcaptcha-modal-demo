28320c6cd69924e3eed5c5f4ea91dc8ab983163f
haji4ref <haji4ref@gmail.com>
2024-10-07 16:06:22 +0330

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div @click="open">Button</div>
+
+  <widget-container-modal />
+</template>
+
+<script>
+import { container, openModal } from "jenesius-vue-modal";
+import ModalContent from "./components/ModalContent.vue";
+
+export default {
+  components: { WidgetContainerModal: container },
+  methods: {
+    open() {
+      openModal(ModalContent);
+    },
+  },
+  created() {
+    const script = document.createElement("script");
+    script.src = "https://widget.arcaptcha.ir/1/api.js?domain=localhost";
+    document.body.appendChild(script);
+  },
+};
+</script>
---
+++ src/components/ModalContent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="background-color: white; width: 500px; height: 500px">
+    <div id="arcaptcha"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.renderCaptcha();
+  },
+  methods: {
+    renderCaptcha() {
+      window.arcaptcha.render(document.getElementById("arcaptcha"), {
+        site_key: "d6ggpvy684",
+      });
+    },
+  },
+};
+</script>
